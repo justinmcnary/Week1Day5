9e1cfd0296cd280d2ec34c304199a02bfb4d89a1
marcy <marcy.montross@gmail.com>
2017-02-03 13:17:22 -0600

    morning work

---
+++ app.js
@@ -0,0 +1,59 @@
+//enter items they need to purchase by entering text and hitting "Return" or clicking the "Add item" button
+//check and uncheck items on the list by clicking the "Check" button
+//permanently remove items from the list
+
+//////////////////////////////////////////////////////////////////////
+
+// 1. create eventListener to capture submit of #shopping-list-entry
+//$('#js-shopping-list-form').submit(function(event) {
+//   event.preventDefault();
+//   addItem(state, $('#shopping-list-entry').val());
+//});
+
+
+
+
+
+
+
+// capture value of "#js-shopping-list-form > input" in a variable
+
+
+
+
+// create itemObj to store item
+// create itemsArr to store each itemObj
+
+// create function to store itemObj to itemsArr
+
+
+
+//
+//var state = {
+//   items: [
+//   // {title:"pizza", done: "true"},
+//   // {title:"pancake mix", done: "false"}
+//   ]
+//};
+//
+//// Functions that modify state
+//
+//var addItem = function(state, item) {
+//   state.items.push(item);
+//};
+//
+//// Functions that render state
+//
+//var renderList = function(state, element) {
+//   var itemsHTML = state.items.map(function(item) {
+//       return '<li>' + item + '</li>';
+//   });
+//   element.html(itemsHTML);
+//};
+//
+//// Event Listeners
+//
+
+
+
+// create function to REMOVE itemObj from itemsArr
