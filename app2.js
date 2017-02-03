0e9677feb9d038d9f91efb9999bedc2d54fd5823
justinmcnary <justinmcnary@yahoo.com>
2017-02-03 12:20:49 -0800

    aftetnoon update

--- app2.js
+++ app2.js
@@ -3,6 +3,7 @@ $('#js-shopping-list-form').submit(function(event) {
    event.preventDefault();
     var input = $(event.currentTarget).find('input[name="shopping-list-entry"]').val();
    addItem(state, input);
+   renderList(state, $('.shopping-list'));
 });
 
 // 2. function to take `input` and put into the object `state`
@@ -18,16 +19,23 @@ var addItem = function(state, item) {
    });
 };
 
+// function toggleCheck(state, item) {
+//     state.items[toggleClass(item)];
+//     class: shopping-item_checked
+// };
+
 // 3. render to screen
-
-
 var renderList = function(state, element) {
-   var itemsHTML = state.items.map(function(item) {
-       var itemHTML = "<li> <span>"+ item + "</span></li>"
+    var itemsHTML = state.items.map(function(item) {
+        return '<li> <span>' 
+                + item.title + 
+                '</span> 
+                <div>
+                <button >
+                <span>check</span></button> </div></li>';
+    });
+    // ele
+    element.html(itemsHTML);
+};
 
-       console.log(itemsHTML);
-//       return '<li>' + item + '</li>';
-   });
-}
-    
     
